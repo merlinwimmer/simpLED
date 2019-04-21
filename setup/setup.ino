ce56153a7b397a13e732549f171e9f8e214cee17
laurinwolf <laurin.wolf@gmx.de>
2019-04-22 00:29:18 +0200

    removed startup animation

--- setup/setup.ino
+++ setup/setup.ino
@@ -3,6 +3,7 @@ void setup() {
     Serial.println("Setup starting...");
     FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
     FastLED.setBrightness(brightness);
+    leds[0] = CRGB(2, 0, 0);
 
     Wire.begin();
 
@@ -13,26 +14,6 @@ void setup() {
     pinMode(BTN_A_PIN, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(BTN_A_PIN), btn_a_ISR, RISING);
     //attachInterrupt(digitalPinToInterrupt(BTN_B_PIN), btn_b_ISR, RISING);  //Isn't build in yet
-
-    for (int i = 0; i < NUM_LEDS; i++) {
-        leds[i] = CRGB(2, 0, 0);
-    }
-    FastLED.show();
-    delay(100);
-    for (int i = 0; i < NUM_LEDS; i++) {
-        leds[i] = CRGB(0, 2, 0);
-    }
-    FastLED.show();
-    delay(100);
-    for (int i = 0; i < NUM_LEDS; i++) {
-        leds[i] = CRGB(0, 0, 2);
-    }
-    FastLED.show();
-    delay(100);
-    for (int i = 0; i < NUM_LEDS; i++) {
-        leds[i] = CRGB(0, 0, 0);
-    }
-    FastLED.show();
 
     Serial.println("Setup done...");
 }
