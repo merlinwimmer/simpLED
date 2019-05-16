cdd67a3412e5877691ef5bc8ed8d34d7f20fd4be
laurinwolf <laurin.wolf@gmx.de>
2019-05-16 22:54:36 +0200

    cooler app cmds at startup

--- setup/setup.ino
+++ setup/setup.ino
@@ -19,6 +19,6 @@ void setup() {
 
     parser("a/a/0/200/50/10");
     apps[0]->setName("Vollbild Farbe");
-    parser("a/a/1/40/3/3/4/5");
+    parser("a/a/1/60/3/3/4/6");
     apps[1]->setName("Schlangen-Animation");
 }
